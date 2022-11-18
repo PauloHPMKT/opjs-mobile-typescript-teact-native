d808dfad2459e2c78b1f8e093a23a29ef8b77817
Paulo Sergio <pauloserg861@gmail.com>
2022-11-18 20:43:28 -0300

    criando cards do menu central do app

--- App.tsx
+++ App.tsx
@@ -1,4 +1,8 @@
 import { useFonts } from 'expo-font';
+
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
 import Main from './src/Main';
 
 export default function App() {
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,6 +1,34 @@
+import { FlatList } from "react-native";
+import { Text } from "../Text";
+import { products } from "../../mocks/products";
+import { Product, ProductImage, ProductDetails, Separator} from './styles';
+import { formatCurrency } from "../../utils/formatCurrency";
+
 const Menu = () => {
 	return(
-		null
+		<FlatList
+			data={products}
+			keyExtractor={product => product._id}
+			style={{ marginTop: 32 }}
+			contentContainerStyle={{ paddingHorizontal: 24 }}
+			ItemSeparatorComponent={Separator}
+			renderItem={({ item: product }) => (
+				<Product>
+					<ProductImage
+						source={{
+							uri: `http://192.168.1.7:19000/uploads/${product.imagePath}`,
+						}}
+					/>
+					<ProductDetails>
+						<Text weight="600">{product.name}</Text>
+						<Text size={14}	color="#666">{product.description}</Text>
+						<Text weight="600" size={14} color="#333" style={{ marginVertical: 8 }}>
+							{formatCurrency(product.price)}
+						</Text>
+					</ProductDetails>
+				</Product>
+			)}
+		/>
 	)
 }
 
