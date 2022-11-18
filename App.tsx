e6fcaffefad2782aa8c95ec86baab36fd6affd04
Paulo Sergio <pauloserg861@gmail.com>
2022-11-17 21:46:03 -0300

    configurando setup

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Fast refresh</Text>
+      <Text>Fast refresh!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
